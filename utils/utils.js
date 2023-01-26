c48671fd98aaab716861c0941d1c3d589a3969b5
Heather Burke <heather.eva93@gmail.com>
2023-01-26 10:39:45 +0000

    be able to communicate with api

---
+++ utils/utils.js
@@ -0,0 +1,22 @@
+require("dotenv").config({ path: __dirname + "/../.env.development" });
+const qs = require("qs");
+const axios = require("axios");
+
+const spotifyAPI = axios.create({ baseURL: "https://api.spotify.com/v1" });
+
+const getMe = async () => {
+  const accessToken = process.env.ACCESS_TOKEN;
+  try {
+    const response = await spotifyAPI.get(`/me/top/tracks`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      data: { scopes: "user-top-read" },
+    });
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getMe();
