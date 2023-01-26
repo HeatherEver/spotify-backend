f11509985c8a50482a42cd8c64dcb9f0530db669
two-triangles <danielsalehguitar@gmail.com>
2023-01-26 19:52:02 +0000

    successfully build connection endpoints

--- app/app.js
+++ app/app.js
@@ -1,3 +1,65 @@
-const express = require("express");
+const express = require("express")
+const cors = require("cors")
+const querystring = require("node:querystring")
+const { default: axios } = require("axios")
 
-const app = express();
+const app = express()
+
+require("dotenv").config({ path: __dirname + "/../.env.development" })
+
+const clientId = process.env.CLIENT_ID
+const clientSecret = process.env.CLIENT_SECRET
+
+app.use(cors())
+
+app.get("/", (req, res, next) => {
+  res.status(200).send({ msg: "hello" })
+})
+
+app.get("/login", (req, res, next) => {
+  const scopes =
+    "user-top-read user-read-email user-read-private user-library-read"
+  const clientId = process.env.CLIENT_ID
+  const queryObj = {
+    response_type: "code",
+    client_id: clientId,
+    redirect_uri: "http://localhost:3000/callback",
+    scope: scopes,
+    show_dialog: true,
+  }
+
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" + querystring.stringify(queryObj)
+  )
+})
+
+app.get("/exchange", (req, res, next) => {
+  const { code } = req.query
+
+  axios
+    .post(
+      "https://accounts.spotify.com/api/token",
+      {
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: "http://localhost:3000/callback",
+      },
+      {
+        headers: {
+          Authorization:
+            "Basic " +
+            Buffer.from(clientId + ":" + clientSecret).toString("base64"),
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    )
+    .then(({ data }) => {
+      process.env.ACCESS_TOKEN = data.access_token
+      res.status(200).send({ access_token: data.access_token })
+    })
+    .catch((err) => {
+      console.log(err, "<-- error")
+    })
+})
+
+module.exports = { app }
--- utils/utils.js
+++ utils/utils.js
@@ -1,22 +1,22 @@
-require("dotenv").config({ path: __dirname + "/../.env.development" });
-const qs = require("qs");
-const axios = require("axios");
+require("dotenv").config({ path: __dirname + "/../.env.development" })
 
-const spotifyAPI = axios.create({ baseURL: "https://api.spotify.com/v1" });
+const axios = require("axios")
 
-const getMe = async () => {
-  const accessToken = process.env.ACCESS_TOKEN;
-  try {
-    const response = await spotifyAPI.get(`/me/top/tracks`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      data: { scopes: "user-top-read" },
-    });
-    console.log(response.data);
-  } catch (error) {
-    console.log(error);
-  }
-};
+const spotifyAPI = axios.create({ baseURL: "https://api.spotify.com/v1" })
 
-getMe();
+// const getMe = async () => {
+//   const accessToken = process.env.ACCESS_TOKEN;
+//   try {
+//     const response = await spotifyAPI.get(`/me/top/tracks`, {
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//       },
+//       data: { scopes: "user-top-read" },
+//     });
+//     console.log(response.data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// getMe();
